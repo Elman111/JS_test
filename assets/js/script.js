b557fce45ae67334d84c748b96633ae86ec1dc13
Elman111 <elmannn@code.edu.az>
2022-01-27 03:15:13 +0400

    JS_test

---
+++ assets/js/script.js
@@ -0,0 +1,89 @@
+// function getSum(n) {
+//     let sum = 0
+//     for (let i = 0; i <= n; i++) {
+//         sum +=i;
+        
+//     }
+//     return sum ;
+// }
+// console.log(getSum(4));
+   
+
+
+// function getSum(n) {
+//     if (n == 0) {
+
+//         return n;
+
+//     }else{
+
+//         return n + getSum(n-1)
+//     } 
+// }
+// console.log(getSum(4));
+
+
+
+// let arr = [1,2,3,4,5]
+
+// function getArrayItems(list) {
+
+//     for (const iterator of list) {
+        
+//         console.log(iterator);
+//     }
+    
+// }
+// getArrayItems(arr);
+
+
+// function createMap(arr,func){
+
+//     let result = [];
+
+//     for (const item of arr) {
+
+//         result.push(func(item))
+//     }
+//     return result;
+// }
+
+// function multiplyTwo(n){
+//     return n * 2
+// }
+
+
+
+
+// console.log(createMap(array,n => n*2));
+
+// let result = array.map(m => m);
+
+// console.log(result);
+
+// array[0] = 33;
+// console.log(array)
+// console.log(result);
+
+
+
+// function test(ar){
+//     return ar;
+// }
+
+// console.log(array.map(m => m *2));
+
+
+//  array.forEach((n,i) => {
+
+//     console.log(n + " " +i);
+
+// });
+
+
+
+
+// let result = array.map((n)=>{
+//     return n ++ ;
+// });
+// console.log(result);
